94243fe4f74c31e81a27c4d02efd2073947b93ca
Gabriel <bielsantacardoso@gmail.com>
2025-04-01 09:10:07 -0300

    declaração e variaveis

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+//DECLARAÇÕES E VARIAVEIS
+var nome ="fiap";
+console.log(nome)
+
+let idade =17;
+console.log(idade)
+
+const Sobrenome ="Cardoso";
+console.log(Sobrenome)
+
+let aula;
+console.log(aula)
+
+let valor =null;
+console.log(valor)
+
+let exemplo1={};
+
+let exemplo2=[];
+
