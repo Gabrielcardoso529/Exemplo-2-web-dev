3868e9700125778edb73bbb820b3d166568b1326
Gabriel <bielsantacardoso@gmail.com>
2025-04-01 09:16:54 -0300

    adicionando tipos de variaveis

--- src/js/script.js
+++ src/js/script.js
@@ -18,3 +18,18 @@ let exemplo1={};
 
 let exemplo2=[];
 
+//TIPOS DE VARIAVES
+let exemplo3 = 10;
+console.log(typeof exemplo3)
+
+let exemplo4= "Aula";
+console.log(typeof exemplo4)
+
+let exemplo5 =true;
+console.log(typeof exemplo5)
+
+let exemplo6 =["duca, malu, biel"];
+console.log(typeof exemplo6)
+
+//CONVERSÕES
+
