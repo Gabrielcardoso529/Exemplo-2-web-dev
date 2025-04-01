a1541530632003b5a3b3b5fa195fd704f56d6ab7
Gabriel <bielsantacardoso@gmail.com>
2025-04-01 09:27:42 -0300

    adicionando conversoes

--- src/js/script.js
+++ src/js/script.js
@@ -28,8 +28,24 @@ console.log(typeof exemplo4)
 let exemplo5 =true;
 console.log(typeof exemplo5)
 
-let exemplo6 =["duca, malu, biel"];
+let exemplo6 =["malu, biel"];
 console.log(typeof exemplo6)
 
 //CONVERSÕES
+
+//float => inteiro
+
+let numFloat =123.456;
+console.log(parseInt(numFloat))
+
+//string => float
+let numString ="547.987"
+console.log(parseFloat(numString))
+
+//float => string
+let numFloat1 =554.665;
+console.log(numFloat1.toString())
+
+let numInt=100;
+console.log(numInt.toString())
 
