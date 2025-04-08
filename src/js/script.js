b9dc72a3bcebcb9afb551a09fb7cdeed8f1c501a
Gabriel <bielsantcardoso@gmail.com>
2025-04-08 09:15:34 -0300

    adicionando estruturas condicionais

--- src/js/script.js
+++ src/js/script.js
@@ -1,91 +1,91 @@
-//DECLARAÇÕES E VARIAVEIS
-var nome ="fiap";
-console.log(nome)
+// //DECLARAÇÕES E VARIAVEIS
+// var nome ="fiap";
+// console.log(nome)
 
-let idade =17;
-console.log(idade)
+// let idade =17;
+// console.log(idade)
 
-const Sobrenome ="Cardoso";
-console.log(Sobrenome)
+// const Sobrenome ="Cardoso";
+// console.log(Sobrenome)
 
-let aula;
-console.log(aula)
+// let aula;
+// console.log(aula)
 
-let valor =null;
-console.log(valor)
+// let valor =null;
+// console.log(valor)
 
-let exemplo1={};
+// let exemplo1={};
 
-let exemplo2=[];
+// let exemplo2=[];
 
-//TIPOS DE VARIAVES
-let exemplo3 = 10;
-console.log(typeof exemplo3)
+// //TIPOS DE VARIAVES
+// let exemplo3 = 10;
+// console.log(typeof exemplo3)
 
-let exemplo4= "Aula";
-console.log(typeof exemplo4)
+// let exemplo4= "Aula";
+// console.log(typeof exemplo4)
 
-let exemplo5 =true;
-console.log(typeof exemplo5)
+// let exemplo5 =true;
+// console.log(typeof exemplo5)
 
-let exemplo6 =["malu, biel"];
-console.log(typeof exemplo6)
+// let exemplo6 =["malu, biel"];
+// console.log(typeof exemplo6)
 
-//CONVERSÕES
+// //CONVERSÕES
 
-//float => inteiro
+// //float => inteiro
 
-let numFloat =123.456;
-console.log(parseInt(numFloat))
+// let numFloat =123.456;
+// console.log(parseInt(numFloat))
 
-//string => float
-let numString ="547.987"
-console.log(parseFloat(numString))
+// //string => float
+// let numString ="547.987"
+// console.log(parseFloat(numString))
 
-//float => string
-let numFloat1 =554.665;
-console.log(numFloat1.toString())
+// //float => string
+// let numFloat1 =554.665;
+// console.log(numFloat1.toString())
 
-let numInt=100;
-console.log(numInt.toString())
+// let numInt=100;
+// console.log(numInt.toString())
 
-//METODOS - PARTE-1
+// //METODOS - PARTE-1
 
-//LENGHT- VERIFICA O TAMANHO DA STRING
+// //LENGHT- VERIFICA O TAMANHO DA STRING
 
-let frase = "O mundo da Tecnologia";
-console.log(frase.length)
+// let frase = "O mundo da Tecnologia";
+// console.log(frase.length)
 
-//indexOf - retorna um trecho de um texto
+// //indexOf - retorna um trecho de um texto
 
-let texto = "Programação Sustentável";
-console.log(texto.indexOf("ç"))
+// let texto = "Programação Sustentável";
+// console.log(texto.indexOf("ç"))
 
-//slice - retorna parte de um texto apontando o inicio e o final
+// //slice - retorna parte de um texto apontando o inicio e o final
 
-let info="Processamento de ponta";
-console.log(info.slice(0,15))
+// let info="Processamento de ponta";
+// console.log(info.slice(0,15))
 
-// OPERADORES ARITMÉTICOS
+// // OPERADORES ARITMÉTICOS
 
-const num1 =10;
-const num2 =20;
+// const num1 =10;
+// const num2 =20;
 
-console.log(num1 + num2)
-console.log(num1 - num2)
-console.log(num1 * num2)
-console.log(num1 / num2)
+// console.log(num1 + num2)
+// console.log(num1 - num2)
+// console.log(num1 * num2)
+// console.log(num1 / num2)
 
 
-// OPERADORES LÓGICOS
+// // OPERADORES LÓGICOS
 
-const num3 =20;
-const num4 =30;
+// const num3 =20;
+// const num4 =30;
 
-console.log(num3 < num4);
-console.log(num3 < num4 && num4 < 50);
-console.log(num3 > num4 || num4 > num3)
-console.log(num4 === num3 || num4 <= num3)
+// console.log(num3 < num4);
+// console.log(num3 < num4 && num4 < 50);
+// console.log(num3 > num4 || num4 > num3)
+// console.log(num4 === num3 || num4 <= num3)
 
 
 
@@ -97,3 +97,50 @@ const num6 =35;
 console.log(num5 == num6);//compara
 console.log(num5 === num6);//compara e verifica o tipo de variavel
 console.log(num5!= num6);//diferente
+
+
+//ESTRUTURA CONDICIONAL
+
+//IF
+
+let valor =100;
+
+if(valor == 100){
+    console.log("é verdadeiro")
+}
+
+//if/else
+
+let valor1 =100;
+
+if(valor == 100){
+    console.log("Valor correto")
+}else{
+    console.log("Valor errado")
+}
+
+//if/else - encadeado ou aninhado
+
+let idade = 71;
+
+if(idade < 14){
+    console.log("Não pode entrar na balada")
+}
+else if(idade >= 14 && idade < 18){
+    console.log("Pode entrar e curtir")
+}
+else if (idade > 50 && idade < 70){
+    console.log("Voçê não tem idade para isso")
+}
+else{
+    console.log("fica em casa assistindo tv")
+}
+
+// Condição ternaria
+// ? = if
+// : = else
+
+let valor2 =300;
+
+let resultado = valor2 == 300 ? "certo": "Errado";
+console.log(resultado)
